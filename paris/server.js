0240a4843ad980a39052aa21da535660c5d6a17f
B-SeungJIn <byeon1031@naver.com>
2022-11-28 17:32:20 +0900

    first commit

--- paris/server.js
+++ paris/server.js
@@ -1,16 +1,23 @@
 const express = require('express');
 const app = express();
-// var dbConfig = require(__dirname + '/config/db.js');
-// var conn = dbConfig.init();
+var dbConfig = require(__dirname + '/config/db.js');
+var conn = dbConfig.init();
 
-// dbConfig.connect(conn);
+dbConfig.connect(conn);
 
-// app.get('/list', function (req, res) {
-//     var sql = 'SELECT * FROM place';
-//     conn.query(sql, function (err, rows, fields) {
-//         if(err) console.log('query is not excuted. select fail...\n' + err);
-//         else res.render('index.ejs', {list : rows});
-//     });
-// });
+app.set('views', './paris/views')
+app.set('view engine', 'ejs');
 
-app.listen(3000, () => console.log('서버 시작'));
+app.get('/', function (req, res) {
+    res.send('메인');
+});
+
+app.get('/List', function (req, res) {
+    var sql = "SELECT * FROM place";
+    conn.query(sql, function (err, rows, fields) {
+        if(err) console.log('query is not excuted. select fail...\n' + err);
+        res.render('index', {list : rows});
+    });
+});
+
+app.listen(8080, () => console.log('포트 8080번에서 서버 시작'));
